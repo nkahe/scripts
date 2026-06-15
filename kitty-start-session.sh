0bff6974141547914bbc541c97bff005155821d1
Henri K <12832691+nkahe@users.noreply.github.com>
2026-06-15 19:15:36 +0300

    session file

--- kitty-start-session.sh
+++ kitty-start-session.sh
@@ -2,7 +2,8 @@
 # Start Kitty with default session. If restoring session is used in KDE,
 # Kitty needs to be blacklisted so only one instance is created.
 
-file=/home/henri/.config/kitty/default.kitty-session
+file=~/.local/state/kitty/sessions/main.kitty-session
+# file=/home/henri/.config/kitty/default.kitty-session
 if [ -f "$file" ]; then
   kitty --session "$file"
 fi
